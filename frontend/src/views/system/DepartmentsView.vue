4a69192fe62b4fb5b88268aecd8d3f68a58d5631
Bùi Vũ Hoàng <buivuhoangwork@gmail.com>
2026-04-21 16:17:52 +0700

    feat: implement DepartmentsView component for managing organizational departments

--- frontend/src/views/system/DepartmentsView.vue
+++ frontend/src/views/system/DepartmentsView.vue
@@ -102,7 +102,7 @@
               d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z" />
           </svg>
           <span class="text-xs text-[#414A4D]/60">
-            <span class="font-semibold text-[#414A4D]">{{ employeeCountMap[dept.id] ?? 0 }}</span> nhân viên
+            <span class="font-semibold text-[#414A4D]">{{ dept.employeeCount ?? employeeCountMap[dept.id] ?? 0 }}</span> nhân viên
           </span>
         </div>
       </div>
@@ -298,7 +298,7 @@ async function fetchDepartments() {
     ])
 
     // Parse departments
-    const rawDept = deptRes.data?.data ?? deptRes.data
+    const rawDept: any = deptRes.data?.data ?? deptRes.data
     departments.value = Array.isArray(rawDept) ? rawDept : (rawDept?.content ?? [])
 
     // Build count map từ employees
